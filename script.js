e49eb239343f42d400a2fbd6d46aef8e30cfe79d
Aditya Nagpal <aditya.nagpal@macys.com>
2018-06-13 22:26:43 +0530

    Assignment 6.2

---
+++ script.js
@@ -0,0 +1,21 @@
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+console.log("Recipe Details"+"\n"+"------------------");
+console.log("Recipe Name: "+recipeName+"\n"+
+            "Preperation Time: "+preparationTime+"\n"+
+            "Recipe By: "+recipeBy+"\n"+
+            "is Vegetarian Dish: "+isVegetarian+"\n"+
+            "Calories: "+calories);
+
+recipeBy="Jimmy Cutter George";
+preparationTime=60;
+
+console.log("Recipe Details After Update"+"\n"+"------------------");
+console.log("Recipe Name: "+recipeName+"\n"+
+            "Preperation Time: "+preparationTime+"\n"+
+            "Recipe By: "+recipeBy+"\n"+
+            "is Vegetarian Dish: "+isVegetarian+"\n"+
+            "Calories: "+calories);
